005879e5b44481d636e042b65bc27d91dad996d6
AnaLauraBelao <analaura.belao+github@outlook.com>
2025-07-25 17:41:08 -0300

    Adding github action config

--- database/migrations/2025_07_17_015114_add_ordem_to_menus_table.php
+++ database/migrations/2025_07_17_015114_add_ordem_to_menus_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('menus', function (Blueprint $table) {
-            //
+            $table->integer('ordem')->default(0);
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('menus', function (Blueprint $table) {
-            $table->integer('ordem')->default(0);
+
         });
     }
 };
