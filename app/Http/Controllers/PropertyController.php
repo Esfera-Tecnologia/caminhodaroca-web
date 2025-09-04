ba13e321b53d8442588ca32d2e3f915fd0646528
willeal17@gmail.com <willeal17@gmail.com>
2025-09-04 10:15:43 -0300

    ajuste na validação dos campos do Google Maps para incluir limites de tamanho

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -197,9 +197,9 @@ class PropertyController extends Controller
             'galeria.*' => ['nullable', 'image'],
             'product_ids' => 'array',
             'product_ids.*' => 'exists:products,id',
-            'google_maps_url' => ['required', 'url'],
-            'latitude' => ['required', 'regex:/^-?\d{1,2}\.\d+$/'],
-            'longitude' => ['required', 'regex:/^-?\d{1,3}\.\d+$/'],
+            'google_maps_url' => ['required', 'url', 'max:2048'],
+            'latitude' => ['required', 'regex:/^-?\d{1,2}\.\d+$/', 'max:15'],
+            'longitude' => ['required', 'regex:/^-?\d{1,3}\.\d+$/', 'max:15'],
         ]);
     }
 
