543ef535174c34f713c3138d86a3aa38ecd1089d
Ryan W. Fonseca <ryanfonseca183@gmail.com>
2025-11-18 15:11:14 -0300

    Debug: Partner Registration

--- app/Http/Requests/RegisterPartnerRequest.php
+++ app/Http/Requests/RegisterPartnerRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests;
 
-use App\Enums\RioDeJaneiroCitiesEnum;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Log;
 
 class RegisterPartnerRequest extends FormRequest
@@ -102,16 +100,5 @@ class RegisterPartnerRequest extends FormRequest
                 'url'
             ],
         ];
-    }
-    public function withValidator($validator)
-    {
-        $validator->after(function ($validator) {
-            if ($validator->fails()) {
-                Log::error('Validation failed in RegisterPartnerRequest', [
-                    'errors' => $validator->errors()->toArray(),
-                    'input' => $this->all(), // opcional, mas útil para debug
-                ]);
-            }
-        });
     }
 }
